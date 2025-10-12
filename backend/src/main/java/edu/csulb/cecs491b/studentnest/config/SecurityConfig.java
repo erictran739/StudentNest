37b74bef44f577cc116c1d53e8124e43368c0710
Puggu-EX <isaacdominguez41@gmail.com>
2025-10-12 16:41:13 -0700

    Modified for secured deployment

--- backend/src/main/java/edu/csulb/cecs491b/studentnest/config/SecurityConfig.java
+++ backend/src/main/java/edu/csulb/cecs491b/studentnest/config/SecurityConfig.java
@@ -1,9 +1,8 @@
 package edu.csulb.cecs491b.studentnest.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,63 +13,66 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import org.springframework.http.HttpMethod;
+import java.util.List;
 
 //this is
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-  @Bean
-  public SecurityFilterChain security(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable());
-    http.cors(Customizer.withDefaults());
-    http.authorizeHttpRequests(auth -> auth
-          .requestMatchers(
-		  "/auth/**",
-		  "/",
-		  "/static/**",
-		  "/css/**",
-		  "/js/**",
-		  "/images/**",
-		  "/login.html",
-		  "/index.html",
-		  "/auth-test.html"
-		  ).permitAll()
-	  .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-          .requestMatchers("/auth/**").authenticated()
-          .anyRequest().authenticated()
-          
-      )
-      .httpBasic(b -> b.disable())
-      .formLogin(f -> f.disable());
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain security(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.cors(Customizer.withDefaults());
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/auth/**",
+                                "/",
+                                "/static/**",
+                                "/css/**",
+                                "/js/**",
+                                "/images/**",
+                                "/login.html",
+                                "/index.html",
+                                "/auth-test.html"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/auth/**").authenticated()
+                        .anyRequest().authenticated()
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-	  return new BCryptPasswordEncoder();
-  }
+                )
+                .httpBasic(b -> b.disable())
+                .formLogin(f -> f.disable());
+        return http.build();
+    }
 
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-      CorsConfiguration cfg = new CorsConfiguration();
-      cfg.setAllowedOriginPatterns(List.of(
-			      "http://localhost:*",
-			      "http://127.0.0.1:*",
-			      "http://puggu.dev:*"
-			      // "http://puggu.dev"
-			      // "http:/127.0.0.1:*",
-			      // "http://192.168.*.*:*"
-			      ));
-      cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
-      cfg.setAllowedHeaders(List.of("*"));
-      cfg.setAllowCredentials(true);
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      source.registerCorsConfiguration("/**", cfg);
-      
-      return source;
-  }
-  
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "http://127.0.0.1:*",
+                "http://puggu.dev:*",
+                "http://puggu.dev",
+                "https://puggu.dev:*",
+                "https://puggu.dev"
+                // "http:/127.0.0.1:*",
+                // "http://192.168.*.*:*"
+        ));
+        cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"));
+        cfg.setAllowedHeaders(List.of("*"));
+        cfg.setExposedHeaders(List.of("*"));
+        cfg.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cfg);
+
+        return source;
+    }
+
 }
