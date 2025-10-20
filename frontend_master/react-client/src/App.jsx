2837dcbe9be8a8de40843b9e8c38432355a8b76e
Simon Tran <smurfylord67@gmail.com>
2025-10-19 23:17:07 -0700

    Downtime page created.

--- frontend_master/react-client/src/App.jsx
+++ frontend_master/react-client/src/App.jsx
@@ -1,36 +1,42 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Register from "./components/Register";
 import AccountSuccess from "./components/AccountSuccess";
 import AccountFailure from "./components/AccountFailure";
+import Downtime from "./components/Downtime";
 
-function App() {
+function RingLayout({ children }) {
   return (
-    <div className="ring" style={{ "--clr": "#ecaa00" }}>
-      <i></i>
-      <i></i>
-      <i></i>
-
-      <div className="login">
-        <Routes>
-          {/* Login */}
-          <Route path="/" element={<Login />} />
-
-          {/* Home (post-login) */}
-          <Route path="/home" element={<Home />} />
-
-          {/* Register */}
-          <Route path="/register" element={<Register />} />
-
-          {/* Registration outcomes */}
-          <Route path="/account-success" element={<AccountSuccess />} />
-          <Route path="/account-failure" element={<AccountFailure />} />
-        </Routes>
-      </div>
+    <div className="ring" style={{ "--clr": "#ff6ec7" }}>
+      <i></i><i></i><i></i>
+      <div className="login">{children}</div>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  const { pathname } = useLocation();
+  const useRing = ["/", "/register", "/account-success", "/account-failure"].includes(pathname);
+
+  return useRing ? (
+    <RingLayout>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/account-success" element={<AccountSuccess />} />
+        <Route path="/account-failure" element={<AccountFailure />} />
+        {/* Fallback */}
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </RingLayout>
+  ) : (
+    <Routes>
+      <Route path="/downtime" element={<Downtime />} />
+      <Route path="/home" element={<Home />} />
+      {/* Safety fallback: if someone hits other routes while not using ring */}
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
+}
